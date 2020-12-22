3947c3c92e8585ed653a4ba45f100f024c344b75
z13z <zgogi15@freeuni.edu.ge>
2020-12-22 22:33:39 +0400

    formated init_tables.sql

--- deployment/docker-images/postgres/init_tables.sql
+++ deployment/docker-images/postgres/init_tables.sql
@@ -1,19 +1,21 @@
 CREATE TYPE KioskImageState AS ENUM ('created', 'waiting', 'done');
 
-CREATE TABLE KioskImage(
-                           id BIGINT GENERATED ALWAYS AS IDENTITY,
-                           name VARCHAR(128) NOT NULL,
-                           create_time TIMESTAMP NOT NULL,
-                           script VARCHAR NOT NULL,
-                           state KioskImageState,
-                           PRIMARY KEY(id)
+CREATE TABLE KioskImage
+(
+    id          BIGINT GENERATED ALWAYS AS IDENTITY,
+    name        VARCHAR(128) NOT NULL,
+    create_time TIMESTAMP    NOT NULL,
+    script      VARCHAR      NOT NULL,
+    state       KioskImageState,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE Kiosk(
-                      id BIGINT GENERATED ALWAYS AS IDENTITY,
-                      name VARCHAR(128) NOT NULL,
-                      kiosk_image_id BIGINT,
-                      create_time TIMESTAMP NOT NULL,
-                      PRIMARY KEY(id),
-                      CONSTRAINT fkIdKioskImage_id FOREIGN KEY (kiosk_image_id) REFERENCES KioskImage(id)
+CREATE TABLE Kiosk
+(
+    id             BIGINT GENERATED ALWAYS AS IDENTITY,
+    name           VARCHAR(128) NOT NULL,
+    kiosk_image_id BIGINT,
+    create_time    TIMESTAMP    NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fkIdKioskImage_id FOREIGN KEY (kiosk_image_id) REFERENCES KioskImage (id)
 );
