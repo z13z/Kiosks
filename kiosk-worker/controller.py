daf45c58ea0940cbfda44a19bca13e932d21210e
z13 <zgogi15@freeuni.edu.ge>
2021-06-23 13:17:15 +0400

    now kiosk can send any kind of file

--- kiosk-worker/controller.py
+++ kiosk-worker/controller.py
@@ -1,15 +1,13 @@
 import logging
-import os
 import subprocess
 
 import flask
 
-LATEST_SCREENSHOT_FILE_NAME = "screenshot.png"
+LATEST_SEND_FILE_NAME = "/home/kiosk/file.png"
 
 
 def get_screenshot():
-    os.system("export DISPLAY=:1 && scrot -z -o {} -e 'mv $f .'".format(LATEST_SCREENSHOT_FILE_NAME))
-    return flask.send_file(LATEST_SCREENSHOT_FILE_NAME)
+    return flask.send_file(LATEST_SEND_FILE_NAME)
 
 
 def execute_command(command):
